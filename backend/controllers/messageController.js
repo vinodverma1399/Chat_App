a2fb06edd330106a35e0274c1424584afc1f5c13
vinodverma1399 <vinodverma2555@gmail.com>
2025-02-04 01:10:16 +0530

    become message controller file

---
+++ backend/controllers/messageController.js
@@ -0,0 +1,8 @@
+export const sendMessage= async (req, res)=>{
+    try {
+        const senderId=req.id;
+        const receiverId=req.Params.id;
+    } catch (error) {
+        console.error(error);
+    }
+}
