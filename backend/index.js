81bd819e4d92e740b2d96370a88b25855020694f
vinodverma1399 <vinodverma2555@gmail.com>
2025-02-04 01:32:20 +0530

    send message

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/database.js";
 import userRoute from "./routes/userRoute.js";
+import messageRoute from "./routes/messageRoute.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config({});
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 //routes
 app.use("/api/v1/user",userRoute);
+app.use("/api/v1/message",messageRoute);
 //http://localhost:8081/api/v1/user/register
 
 app.listen(PORT,()=>{
