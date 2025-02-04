8f71289df57ac0548ede7317f3435e978214f1f0
vinodverma1399 <vinodverma2555@gmail.com>
2025-02-04 17:52:34 +0530

    messager controller add

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,7 +1,35 @@
+import { Conversation } from "../models/conversationModel.js";
+import { Message } from "../models/messageModel.js";
+
 export const sendMessage= async (req, res)=>{
     try {
         const senderId=req.id;
-        const receiverId=req.Params.id;
+        const receiverId=req.params.id;
+        const {message} =req.body;
+        let gotConversation= await Conversation.findOne({
+            participants:{$all : [senderId,receiverId] }
+        }); 
+
+        if(!gotConversation){
+            gotConversation=await Conversation.create({
+                participants:[senderId,receiverId]
+            })
+        } 
+        const newMessage =await Message.create({
+            senderId,
+            receiverId,
+            message
+        })
+
+        if(newMessage){
+            gotConversation.message.push(newMessage._id)
+        };
+        await gotConversation.save();
+
+        //Soket IO
+        return res.status(201).json({
+            message:"message send successfully"
+        })
     } catch (error) {
         console.error(error);
     }
